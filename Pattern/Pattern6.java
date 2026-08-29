d4412300ed013c7090e0ac0866ab7791aa36b439
Shashi <shashimanidwivedi6@gmail.com>
2026-08-29 09:03:55 +0530

    Pattern

---
+++ Pattern/Pattern6.java
@@ -0,0 +1,66 @@
+public class Pattern6 {
+    public static void main(String[]args){
+        int n=7;
+        // for(int i=1;i<=n;i++){
+            
+        //     for (int j = 0; j < i; j++) {
+        //         char ch = 'A';
+        //         System.out.printf("%c ", ch + j);
+        //     }
+            
+        //     System.out.println();
+        // }
+        // for(int i=n;i>=0;i--){
+            
+        //     for (int j = 0; j < i; j++) {
+        //         char ch = 'A';
+        //         System.out.printf("%c ", ch + j);
+        //     }
+            
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=n;i++){
+            
+        //     for (int j = 0; j < n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j < i; j++) {
+        //         char ch = 'A';
+        //         System.out.printf("%c", ch + j);
+        //     }
+            
+            
+        //     System.out.println();
+        // }
+
+        for(int i=n;i>=1;i--){
+            
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                char ch = 'A';
+                System.out.printf("%c", ch + j);
+            }
+            
+            
+            System.out.println();
+        }
+
+        for(int i=2;i<=n;i++){
+            
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                char ch = 'A';
+                System.out.printf("%c", ch + j);
+            }
+            
+            
+            System.out.println();
+        }
+    }
+    
+}
